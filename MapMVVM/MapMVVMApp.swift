134333c815f26642b273d59653101a8c5e8141f2
tom <chings228@gmail.com>
2023-07-18 17:09:54 +0800

    Initial Commit

---
+++ MapMVVM/MapMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapMVVMApp.swift
+//  MapMVVM
+//
+//  Created by man ching chan on 18/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
